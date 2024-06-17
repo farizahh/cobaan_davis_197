5a3670d52e445333f9d0b5bb873fee4fa6f2e676
farizahh <90184606+farizahh@users.noreply.github.com>
2024-06-17 20:13:36 +0700

    commit

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 # Nama file CSV
 fn1 = 'imdb_top.csv'
+
+# Menampilkan judul di halaman web
+st.title("Scraping Website IMDB")
     
 # Membaca file CSV ke dalam DataFrame dengan encoding 'latin1'
 df1 = pd.read_csv(fn1, encoding='latin1')
-
-# Judul aplikasi
-st.title("Scraping Website IMDB")
 
 # Menampilkan DataFrame sebagai tabel
 df1
