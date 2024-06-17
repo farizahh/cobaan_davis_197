7d9e45b37131ababf01be87759fd24d825386673
farizahh <90184606+farizahh@users.noreply.github.com>
2024-06-17 20:05:24 +0700

    commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
 import pandas as pd
 
 # Nama file CSV
@@ -7,5 +6,5 @@ fn1 = 'imdb_top.csv'
 # Membaca file CSV ke dalam DataFrame dengan encoding 'latin1'
 df1 = pd.read_csv(fn1, encoding='latin1')
 
-# Menampilkan DataFrame sebagai tabel di aplikasi web
-st.dataframe(df1)
+# Menampilkan DataFrame sebagai tabel
+df1
